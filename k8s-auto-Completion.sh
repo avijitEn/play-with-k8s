1f72895c2fea75fac6485154898dc5df80888fa4
Avijit Majumder <59084432+avijitEn@users.noreply.github.com>
2020-08-07 17:39:59 +0530

    Update k8s-auto-Completion.sh

--- k8s-auto-Completion.sh
+++ k8s-auto-Completion.sh
@@ -2,3 +2,4 @@ yum install bash-completion -y
 echo 'source <(kubectl completion bash)' >>~/.bashrc
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -F __start_kubectl k' >>~/.bashrc
+source >>~/.bashrc
