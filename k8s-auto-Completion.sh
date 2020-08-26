37e82470e5e660726a255dc633b0a991a68092f1
Avijit Majumder <59084432+avijitEn@users.noreply.github.com>
2020-08-26 20:15:57 +0530

    Update k8s-auto-Completion.sh

--- k8s-auto-Completion.sh
+++ k8s-auto-Completion.sh
@@ -5,3 +5,4 @@ echo 'source <(kubectl completion bash)' >>~/.bashrc
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -F __start_kubectl k' >>~/.bashrc
 source ~/.bashrc
+echo "auto-Completion completed"
